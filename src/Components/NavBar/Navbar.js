c6cfc18aa6c1b23814028a53698311b34ab2f243
munish-garg <munishkumar123264@gmail.com>
2021-04-05 20:17:55 +0530

    navbar1

---
+++ src/Components/NavBar/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './navbar.css'
+
+const Navbar = () =>{
+    return (
+        <>
+            <header>
+                <nav>
+                    <ul id="navigation-links">
+                        <li>
+                            <a href="">
+                                <i class="fas fa-question"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">Quiz App</a>
+                        </li>
+                        <li><a href="">Login</a></li>
+                        <li><a href="">SignUp</a></li>
+                    </ul>       
+                </nav>
+            </header>
+        </>
+    );
+}
+
+export default Navbar;
