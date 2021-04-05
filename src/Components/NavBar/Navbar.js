b4d26e6d1cade281a19f422452247a6801812900
munish-garg <munishkumar123264@gmail.com>
2021-04-05 23:38:26 +0530

    navbarIssueResolved

--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -8,14 +8,8 @@ const Navbar = () =>{
             <header>
                 <nav>
                     <ul id="navigation-links">
-                        <li>
-                            <a href="">
-                                <i class="fas fa-question"></i>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="">Quiz App</a>
-                        </li>
+                        <li><a href=""><i class="fas fa-question"></i></a></li>
+                        <li><a href="">Quiz App</a></li>
                         <li><a href="">Login</a></li>
                         <li><a href="">SignUp</a></li>
                     </ul>       
